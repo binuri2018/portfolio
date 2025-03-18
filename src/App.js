1873e860444a8a5d3fefca454d4c8bb1458c3095
binuri2018 <imbinuri@gmail.com>
2025-03-18 15:30:47 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import Education from "./components/Education";
@@ -20,23 +21,44 @@ function App() {
         <div className="content">
           <h1>Hi, I'm Binuri</h1>
           <p>Full Stack Developer || Information Technology Undergraduate</p>
+
+          {/* Social Media Links */}
+          <div className="social-links">
+            <a
+              href="https://www.linkedin.com/in/binuri-manodya/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-btn"
+            >
+              <FaLinkedin />
+            </a>
+
+            <a
+              href="https://github.com/binuri2018"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-btn"
+            >
+              <FaGithub />
+            </a>
+          </div>
+
           <button className="download-btn">Download CV</button>
         </div>
       </div>
 
       <div className="content-section1">
-      <Element name="about">
-        <AboutMe />
-      </Element>
-      <Education/>
-      <Skills/>
-      <Element name="projects">
-        <Projects />
-      </Element>
-      <Element name="contact">
-        <Contact />
-      </Element>
-
+        <Element name="about">
+          <AboutMe />
+        </Element>
+        <Education />
+        <Skills />
+        <Element name="projects">
+          <Projects />
+        </Element>
+        <Element name="contact">
+          <Contact />
+        </Element>
       </div>
     </div>
   );
